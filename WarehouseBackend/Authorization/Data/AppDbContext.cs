ea1e7813b65214fa59d6ebbe875bc0677ce9cf92
Bihari Csaba <biharicsaba83@gmail.com>
2025-04-03 06:28:06 +0200

    User authorization work in progress

---
+++ WarehouseBackend/Authorization/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Authorization.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authorization.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string conn = "Server=localhost;Port=3306;Database=WarehouseAuth;user=root;password=password";
+
+                optionsBuilder.UseMySQL(conn);
+            }
+        }
+    }
+}
